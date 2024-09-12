6f54e8347c6a5b5e818748784cfc55406042f69a
siriusbrack2212 <149822163+siriusbrack2212@users.noreply.github.com>
2024-09-12 23:21:47 +0000

    comit 2
    nessa parte do projeto ja começei a home

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template
 import requests
-
 
 app = Flask(__name__)
 
 # Configurações do Airtable
-AIRTABLE_BASE_ID = 'appNwO27axKgEb6w7'  # Substitua com o ID da sua base
-AIRTABLE_TABLE_NAME = 'Products'         # Substitua com o nome da sua tabela
-AIRTABLE_API_KEY = 'pat2QwwpmKHkee7GL.c28ad5f7dddda86a63b8aa20ed404d454f399cea45a0d08becf8e1f14a7d861c'  # Substitua com o seu token de acesso pessoal
+AIRTABLE_BASE_ID = 'appNwO27axKgEb6w7'  # ID da base do Airtable
+AIRTABLE_TABLE_NAME = 'Products'         # Nome da tabela do Airtable
+AIRTABLE_API_KEY = 'pat2QwwpmKHkee7GL.c28ad5f7dddda86a63b8aa20ed404d454f399cea45a0d08becf8e1f14a7d861c'  # Token de acesso pessoal
 
 # URL da API do Airtable
 AIRTABLE_API_URL = f'https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/{AIRTABLE_TABLE_NAME}'
@@ -20,12 +19,17 @@ HEADERS = {
 
 @app.route('/')
 def index():
-    response = requests.get(AIRTABLE_API_URL, headers=HEADERS)
-    if response.status_code == 200:
+    try:
+        response = requests.get(AIRTABLE_API_URL, headers=HEADERS)
+        response.raise_for_status()  # Lança uma exceção para códigos de erro HTTP
         data = response.json()
-        return jsonify(data)  # Retorna os dados em formato JSON
-    else:
-        return jsonify({'error': 'Failed to fetch data from Airtable'}), response.status_code
+        products = data.get('records', [])
+        return render_template('home.html', products=products)
+    except requests.exceptions.RequestException as e:
+        app.logger.error(f"Request failed: {e}")
+        return "Failed to fetch data from Airtable", 500
 
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
