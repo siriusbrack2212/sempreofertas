1d832b01abc0bb2fa0591d1d7146bdd14c47f76f
siriusbrack2212 <149822163+siriusbrack2212@users.noreply.github.com>
2024-09-12 22:59:49 +0000

    comit 1

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
-from flask import Flask, render_template, request, redirect, url_for
-from airtable import Airtable
+from flask import Flask, jsonify
+import requests
+
 
 app = Flask(__name__)
 
+# Configurações do Airtable
+AIRTABLE_BASE_ID = 'appNwO27axKgEb6w7'  # Substitua com o ID da sua base
+AIRTABLE_TABLE_NAME = 'Products'         # Substitua com o nome da sua tabela
+AIRTABLE_API_KEY = 'pat2QwwpmKHkee7GL.c28ad5f7dddda86a63b8aa20ed404d454f399cea45a0d08becf8e1f14a7d861c'  # Substitua com o seu token de acesso pessoal
 
-AIRTABLE_BASE_ID = 'your_base_id'
-AIRTABLE_TABLE_NAME = ''
-AIRTABLE_API_KEY = 'pat2QwwpmKHkee7GL.26cd860c6cbdef70f731fb8893ed731f0b9c13df61f8de83d551242f635e28bd'
+# URL da API do Airtable
+AIRTABLE_API_URL = f'https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/{AIRTABLE_TABLE_NAME}'
 
-airtable = Airtable(AIRTABLE_BASE_ID, AIRTABLE_TABLE_NAME, AIRTABLE_API_KEY)
+# Cabeçalhos para autenticação e tipo de conteúdo
+HEADERS = {
+    'Authorization': f'Bearer {AIRTABLE_API_KEY}',
+    'Content-Type': 'application/json'
+}
+
+@app.route('/')
+def index():
+    response = requests.get(AIRTABLE_API_URL, headers=HEADERS)
+    if response.status_code == 200:
+        data = response.json()
+        return jsonify(data)  # Retorna os dados em formato JSON
+    else:
+        return jsonify({'error': 'Failed to fetch data from Airtable'}), response.status_code
+
+
+
